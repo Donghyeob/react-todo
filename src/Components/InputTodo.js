8c0e17dbaf3add0ca4f836b583f0b3824a42ea4a
Donghyeob Kang <donghyeob.k@gmail.com>
2021-04-24 02:23:55 +0900

    create and delete

--- src/Components/InputTodo.js
+++ src/Components/InputTodo.js
@@ -4,29 +4,41 @@ import ItemList from './ItemList';
 const InputTodo = () => {
   const [inputText, setInputText] = useState('');
   const [todoItem, setTodoItem] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [checkedItem, setCheckedItem] = useState([]);
   const inputRef = useRef();
 
   const onChangeInput = (e) => {
     setInputText(e.target.value);
   }
   const addTodo = () => {
+    setCounter(counter + 1);
     !inputText
-      ? setTodoItem([...todoItem, '데이터가 없음'])
-      : setTodoItem([...todoItem, inputText]);
+      ? setTodoItem([...todoItem, { id: counter.toString(), text: '데이터가 없음' }])
+      : setTodoItem([...todoItem, { id: counter.toString(), text: inputText }]);
     setInputText('');
     inputRef.current.focus();
+  }
+  const delTodo = () => {
+    console.log(checkedItem.slice());
+    console.log(todoItem);
+    setTodoItem(
+      todoItem.filter(
+        check => !checkedItem.includes(check.id)
+      )
+    );
   }
 
   return (
     <>
       <input value={inputText} ref={inputRef} onChange={onChangeInput} />
       <button onClick={addTodo}>Add</button>
-      <button>Del</button>
+      <button onClick={delTodo}>Del</button>
       <div>
         {/* {todoItem.map((data, index) => (
           <li key={index}>{data}</li>
         ))} */}
-        <ItemList data={todoItem} />
+        <ItemList data={todoItem} setCheckedItem={setCheckedItem} />
       </div>
     </>
   )
--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import Item from './Item';
+import React, { useEffect, useState } from 'react';
 
-const ItemList = ({ data }) => {
-  console.log("ItemList");
-  console.log(data);
+const ItemList = ({ data, setCheckedItem }) => {
+  // console.log(data);
+  const [checked, setChecked] = useState([]);
+
+  const clickCheck = (e) => {
+    if (e.target.checked) {
+      setChecked([...checked, e.target.value]);
+    } else {
+      setChecked(checked.filter(check => check !== e.target.value));
+    }
+    // console.log(checked);
+  }
+
+  useEffect(() => {
+    // console.log(checked);
+    setCheckedItem(checked);
+  }, [clickCheck])
+
   return (
     <>
-      <Item data={data} />
+      <ul>
+        {data.map((data) => (
+          <li key={data.id} style={{ listStyleType: 'none' }}>
+            <input type='checkbox' value={data.id} onClick={clickCheck} />{data.text}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
